427b28729d14c8ff10f604a453a7dfe287c6b44f
Tomasz <33938125+tomaszkubat@users.noreply.github.com>
2019-09-13 21:22:14 +0200

    Update implement.scala

--- implement.scala
+++ implement.scala
@@ -13,6 +13,8 @@ class XlsxFile(file: File, docType: String, sheetName: String, contentToWrite: A
     val sheetName = ... construct sheeet name
     this(dqaFile.file, dqaFile.docType, sheetName, dqaFile.content)
   }
+  
+  def
 
   override def toString: String = {
     return ("%s opis %s opis %d cyfra).format(dataType, cos, 1)
@@ -20,6 +22,11 @@ class XlsxFile(file: File, docType: String, sheetName: String, contentToWrite: A
 
 }
 
+
+def exists() {
+  // use for the initial creation for all the documents
+
+}
 
 
 }
