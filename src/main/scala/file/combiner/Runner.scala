4bcb014cc256ab6b0b1f5a40914fa0f48977d1c3
Tomasz <33938125+tomaszkubat@users.noreply.github.com>
2019-09-18 09:41:58 +0200

    Update Runner.scala

--- src/main/scala/file/combiner/Runner.scala
+++ src/main/scala/file/combiner/Runner.scala
@@ -1,11 +1,29 @@
 package file.combiner
 
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.{SparkConf,SparkContext}
+
 import file.combiner.utils.Loggable
 
 
 /**
 *
 */
-object Runner {
+object Runner extends Loggable {
 
+  /** */
+  def main(args: Array[String]): Unit = {
+   
+    val sparkConf = new SparkConf()
+      .setMaster("local")
+      .setAppName("File_Combiner")
+    
+    // inicialize SparkContext and sparkSqlContext
+    implicit val sc: SparkContext = new SparkContext(sparkConf)
+    implicit val sq: SQLContext = new SQAContext(sc)
+    
+    
+    
+  }
+  
 }
