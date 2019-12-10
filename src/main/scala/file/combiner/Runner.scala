e4105c2261b578b8c4831f4b04b19ea56971ac08
TomaszKubat <tomasz.kubat@wp.pl>
2019-12-10 21:07:03 +0100

    minor changes in Runner

--- src/main/scala/file/combiner/Runner.scala
+++ src/main/scala/file/combiner/Runner.scala
@@ -1,41 +1,39 @@
 package file.combiner
 
+import com.typesafe.config.ConfigFactory
 import org.apache.spark.sql.SQLContext
-import org.apache.spark.{SparkConf,SparkContext}
-// import com.typesafe.config.ConfigFactory
-
+import org.apache.spark.{SparkConf, SparkContext}
 import java.io.File
-
 import file.combiner.logging.Logger
 
 
 /**
-*
+* @author tomasz.kubat
 */
 object Runner extends Logger {
 
   /**
-    *
+    * Main
+    * @param first param is a path to the property file
     */
   def main(args: Array[String]): Unit = {
 
     warn(s"Starting application")
 
-//    val propertyPath = if(!args.isEmpty) args(0) else "src/main/resources/configuration.properties"
-//
-//
-//    val config = ConfigFactory.parseFile(new File(propertyPath))
-//    val a = config.getString("rawDataPath")
-//
-//
-//    val sparkConf = new SparkConf()
-//      .setMaster("local")
-//      .setAppName("File_Combiner")
-//
-//    // inicialize SparkContext and sparkSqlContext
-//    implicit val sc: SparkContext = new SparkContext(sparkConf)
-//    implicit val sq: SQLContext = new SQAContext(sc)
-//
+    /** load configuration */
+    val propertyPath = if(!args.isEmpty) args(0) else "src/main/resources/configuration.properties"
+
+    val config = ConfigFactory.parseFile(new File(propertyPath))
+    val a = config.getString("rawDataPath")
+
+
+    /** inicialize Spark */
+    val sparkConf = new SparkConf().setMaster("local").setAppName("File_Combiner")
+    val sc: SparkContext = new SparkContext(sparkConf)
+    val sq: SQLContext = new SQLContext(sc)
+
+
+    warn(s"Application finished")
 
   }
   
